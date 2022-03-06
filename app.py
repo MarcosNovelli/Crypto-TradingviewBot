21e8e5300194ebd3f7d537149d010694d20134c4
Marcos Novelli <marcossnovelli@gmail.com>
2022-03-06 04:07:00 -0300

    solved problem

--- app.py
+++ app.py
@@ -12,7 +12,11 @@ client = Client(config.API_KEY, config.API_SECRET, tld='com')
 def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
     try:
         print(f"sending order {order_type} - {side} {quantity} {symbol}")
-        order = client.futures_create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+        if symbol == 'BTCUSDT':
+            limited_qty =  "{:.2f}".format(quantity)
+        else:
+            limited_qty = "{:.3f}".format(quantity)
+        order = client.futures_create_order(symbol=symbol, side=side, type=order_type, quantity=limited_qty)
         client.futures_change_leverage(symbol=symbol, leverage=10)
     except Exception as e:
         print("an exception occured - {}".format(e))
