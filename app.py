5dffade41f89910eabc02af96bd5d82bc8e3d737
Marcos Novelli <marcossnovelli@gmail.com>
2022-03-06 04:19:15 -0300

    finally finished changing everything

--- app.py
+++ app.py
@@ -11,13 +11,14 @@ client = Client(config.API_KEY, config.API_SECRET, tld='com')
 
 def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
     try:
-        print(f"sending order {order_type} - {side} {quantity} {symbol}")
         if symbol == 'BTCUSDT':
             limited_qty =  "{:.2f}".format(quantity)
         else:
             limited_qty = "{:.3f}".format(quantity)
         order = client.futures_create_order(symbol=symbol, side=side, type=order_type, quantity=limited_qty)
         client.futures_change_leverage(symbol=symbol, leverage=10)
+        
+        print(f"sending order {order_type} - {side} {limited_qty} {symbol}")
     except Exception as e:
         print("an exception occured - {}".format(e))
         return False
